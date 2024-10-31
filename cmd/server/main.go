e94abe04134558e94ee593c0c2030b762f036d64
Rishikesh Sadanandan <rishikesh.sadanandan@gmail.com>
2024-10-31 15:52:53 -0500

    attempting to set up simple fileserver - 🚫 failed

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,14 +3,24 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"path/filepath"
 )
 
 func main() {
-	// register request handler
+	// Register request handler for the root path
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Welcome to kaylaconsta.com. You're currently visiting: %s\n", r.URL.Path)
 	})
 
-	// listen for HTTP connections
-	http.ListenAndServe(":42069", nil)
+	// Set up the static directory
+	staticDir := filepath.Join("..", "..", "web", "static")
+	fmt.Println("Static directory:", staticDir) // Log the static directory path
+
+	fs := http.FileServer(http.Dir(staticDir))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+
+	// Listen for HTTP connections
+	if err := http.ListenAndServe(":42069", nil); err != nil {
+		fmt.Println("Error starting server:", err)
+	}
 }
